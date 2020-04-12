4926334015cfc1deea932567317928fbe781413a
yezanrafed <yezan.rafed@tagpay.fr>
2020-04-12 12:41:43 +0200

    Create navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../entities/user';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  isCollapsed: boolean = false;
+
+  navItems: Array<any> = [
+    {link: '/home', text: 'Home'},
+    {link: '/todolists', text: 'Todolists'},
+    {link: '/friends', text: 'Friends'},
+  ]
+
+  user: User;
+
   constructor() { }
 
   ngOnInit(): void {
-    
+    this.loadUser();
   }
 
+  loadUser(): void {
+    let user: User = new User();
+    user.firstName = "Yezan";
+    user.lastName = "Rafed";
+    user.email = "y.rafed@gmail.com";
+    user.username = "yezanr";
+    this.user = user;
+  }
 }
