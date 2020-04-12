94ba65cc1858d8afd5b7e21db63b8a16a76de462
yezanrafed <yezan.rafed@tagpay.fr>
2020-04-12 14:25:59 +0200

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { TodolistsPageComponent } from './pages/todolists-page/todolists-page.component';
